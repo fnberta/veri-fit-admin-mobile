0e4d132591bf13849089c57c7a3be897a9406525
Fabio Berta <fnberta@gmail.com>
2023-01-21 22:41:45 +0100

    update android gradle plugin

--- gradle/plugins/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ gradle/plugins/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -15,7 +15,6 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 33
                 defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 defaultConfig.consumerProguardFiles("consumer-rules.pro")
             }
