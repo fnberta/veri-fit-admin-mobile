abad52a651b5789d3166dd08dc23ce41c52a5561
Fabio Berta <fnberta@gmail.com>
2023-02-21 22:57:51 +0100

    add plugin to update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.jvm) apply false
@@ -5,4 +7,22 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.updates)
+    alias(libs.plugins.catalog.update)
+}
+
+versionCatalogUpdate {
+    keep {
+        keepUnusedLibraries.set(true)
+        keepUnusedPlugins.set(true)
+    }
+}
+
+tasks.named("dependencyUpdates", DependencyUpdatesTask::class).configure {
+    val immaturityLevels = listOf("rc", "cr", "m", "beta", "alpha", "preview")
+    val immaturityRegexes = immaturityLevels.map { ".*[.\\-]$it[.\\-\\d]*".toRegex(RegexOption.IGNORE_CASE) }
+
+    fun immaturityLevel(version: String): Int = immaturityRegexes.indexOfLast { version.matches(it) }
+
+    rejectVersionIf { immaturityLevel(candidate.version) > immaturityLevel(currentVersion) }
 }
