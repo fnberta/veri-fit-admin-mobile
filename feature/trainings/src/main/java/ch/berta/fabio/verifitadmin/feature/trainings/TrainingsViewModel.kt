e0bc23a34bc615541bb5b31a63fa40e14f377dc2
Fabio Berta <fnberta@gmail.com>
2023-04-10 22:19:07 +0200

    remove indirection

--- feature/trainings/src/main/java/ch/berta/fabio/verifitadmin/feature/trainings/TrainingsViewModel.kt
+++ feature/trainings/src/main/java/ch/berta/fabio/verifitadmin/feature/trainings/TrainingsViewModel.kt
@@ -3,6 +3,7 @@ package ch.berta.fabio.verifitadmin.feature.trainings
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.berta.fabio.verifitadmin.data.trainings.Training
+import ch.berta.fabio.verifitadmin.data.trainings.TrainingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
@@ -17,8 +18,10 @@ internal sealed class TrainingsUiState {
 }
 
 @HiltViewModel
-internal class TrainingsViewModel @Inject constructor(getTrainings: GetTrainings) : ViewModel() {
-    val uiState = getTrainings()
+internal class TrainingsViewModel @Inject constructor(
+    trainingsRepository: TrainingsRepository
+) : ViewModel() {
+    val uiState = trainingsRepository.getTrainings()
         .map<_, TrainingsUiState> { TrainingsUiState.Content(it) }
         .catch { emit(TrainingsUiState.Error) }
         .stateIn(
