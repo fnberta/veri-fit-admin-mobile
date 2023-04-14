dc315ef7e1fc97ad2b9a73e2674455895335e0c7
Fabio Berta <fnberta@gmail.com>
2023-04-14 22:27:54 +0200

    update gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         }
     }
     kotlinOptions { jvmTarget = "1.8" }
-    packagingOptions { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
 tasks.named("build") { dependsOn(project(":").tasks.named("installPreCommitHook")) }
